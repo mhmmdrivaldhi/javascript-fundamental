2b1847b53bca25cb99d60ea107fbe598d132fe22
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-12-15 23:22:47 +0700

    [Variable] let, var, const

---
+++ variable/script.js
@@ -0,0 +1,52 @@
+// 1. variable using let
+
+// create varible
+let name = "Muhammad Rivaldhi"
+// showing the output on console
+console.log(name); 
+
+// change the value of variable
+name = "Sugiono"
+// showing the output on console
+console.log(name)
+
+
+// 2. variable using var
+
+// create variable
+var firstName = "Muhammad "
+// showing the output on console
+console.log(firstName)
+
+// create variable
+var lastName = "Rivaldhi"
+// showing the output on console
+console.log(lastName)
+
+// create variable 
+var age = 21
+// showing the output on console
+console.log(age)
+
+// behavior of let
+
+let greet = "Hello"
+{
+    let greet = "World!"
+    console.log(greet)
+}
+console.log(greet)
+
+// behavior of var
+
+var reply = "you"
+{
+    var reply = "too"
+    console.log(reply)
+}
+console.log(reply)
+
+// 3. variable using const
+
+const BirthYear = 2004
+console.log(BirthYear)
