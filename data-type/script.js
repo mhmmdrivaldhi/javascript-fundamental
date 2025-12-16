3b3ae456297473ed9bd655ad606eeedcbaca5edb
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-12-16 22:03:40 +0700

    [Data type] string, number, boolean, null, undefined

---
+++ data-type/script.js
@@ -0,0 +1,24 @@
+// string data type
+let nameString = "John"
+console.log(nameString)
+console.log(typeof nameString)
+
+// number data type
+var ageNumber = 21
+console.log(ageNumber)
+console.log(typeof ageNumber)
+
+// boolean data type
+var isMarried = false
+console.log(isMarried)
+console.log(typeof isMarried)
+
+// null data type
+let empty = null
+console.log(empty)
+console.log(typeof empty)
+
+// undefined data type
+let nothing 
+console.log(nothing)
+console.log(typeof nothing)
