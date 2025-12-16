a6213b42980fd1491b7b5b2eaaf624aeb90ef660
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-12-16 23:17:22 +0700

    [Strings] Escaping & Literal

---
+++ string-literal-escaping/script.js
@@ -0,0 +1,28 @@
+let dataString = "Hello World!"
+console.log(dataString)
+
+// Escaping Strings
+let escString = "I'm a \"Muhammad Rivaldhi\" string!"
+console.log(escString)
+
+let escString2 = "I'm \"Not Married\" For Now"
+console.log(escString2)
+
+let escString3 = `Slowly But Sure "I will Understand" About "Javascript"`
+console.log(escString3)
+
+let escString4 = "After i mastered 'Basic Javascript', i will to learn framework of javascript, like: \n Node.js, Express.js, and React.js"
+console.log(escString4)
+
+// Literal Strings (Template String)
+let firstName = "Muhammad"
+let lastName = "Rivaldhi"
+let fullName = `My First Name's ${firstName} and my Last Name's ${lastName}`
+console.log(fullName)
+
+let age = 21
+let birthDate = "10 Juli 2004"
+let bio = `My Age's ${age} and my Birth Date's ${birthDate}`
+console.log(bio)
+
+
